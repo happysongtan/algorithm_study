01c1553b3e94c655c2a0610309f94d6cde0b1720
happysongtan <xorb4488@gmail.com>
2026-03-24 20:40:29 +0900

    feat: 의상 조합 문제 코드

---
+++ hash/clothes_Select.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int solution(String[][] clothes) {
+        int sum=1;
+        HashMap <String, Integer> map=new HashMap<>();
+       for(int i=0;i<clothes.length;i++){
+        map.put(clothes[i][1],map.getOrDefault(clothes[i][1],0)+1);
+       }
+        for(int value:map.values()){  
+                sum=sum*(value+1);       
+        }
+        return sum-1;
+    }
+}
