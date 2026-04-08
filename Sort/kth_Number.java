0e604980e12e109234906ed73100e7a54031f9b7
happysongtan <xorb4488@gmail.com>
2026-04-08 20:52:42 +0900

    [Programmers Sort]k번째 수

---
+++ Sort/kth_Number.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int[] solution(int[] array, int[][] commands) {
+        int[] answer =new int[commands.length];
+        for(int i=0;i<commands.length;i++){
+        int[]sliced=Arrays.copyOfRange(array,commands[i][0]-1,commands[i][1]);
+           Arrays.sort(sliced);
+            answer[i]=sliced[commands[i][2]-1];
+        }
+        
+        return answer;
+    }
+}
