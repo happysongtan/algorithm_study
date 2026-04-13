9d81410ab2bd7e650abcbd95728abc26f8650965
happysongtan <xorb4488@gmail.com>
2026-04-13 21:16:30 +0900

    [Programmers BruteForce]소수 찾기
    -재귀함수 사용해서 풀이해야함

---
+++ BruteForce/decimal.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Solution {
+    public void diff(){
+        for(int i=0;i<;i++){
+            if(visited[i]==false){
+                visited[i]==true;
+                dfs(current+number[i]);
+                visited[i]=false;
+            }
+        }
+    }
+    public int solution(String numbers) {
+        boolean visited=new boolean<>();
+        int answer = 0;
+        return answer;
+    }
+}
