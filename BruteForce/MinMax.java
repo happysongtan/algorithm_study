41cce24a455b4468d6188410aefdd6c9ea130f25
happysongtan <xorb4488@gmail.com>
2026-04-29 21:19:04 +0900

    [Programmers BruteForce] 최대값 최솟값 code

---
+++ BruteForce/MinMax.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String solution(String s) {
+        String []arr=s.split(" ");
+        int min= Integer.parseInt (arr[0]);
+        int max=min;
+        for(int i=1;i<arr.length;i++){
+            int num=Integer.parseInt (arr[i]);
+            if(num<min){
+                min=num;
+            }
+            if(num>max){
+                max=num;
+            }
+        }
+        String answer = ""+min+" "+max;
+        return answer;
+    }
+}
